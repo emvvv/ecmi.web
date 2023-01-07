f175ca32ff641596e2382c04a5ffc5dd9b50d0cb
Mary Valera <emvalera356@gmail.com>
2023-01-07 22:57:39 +0800

    add build folder

---
+++ build/precache-manifest.37a4f12bc991f5992db2df44a63c4895.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7be9b218b0026a5cc3701ce752fb8996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f771154c47dddb3c49a7",
+    "url": "/static/css/main.09c00347.chunk.css"
+  },
+  {
+    "revision": "4a0487a7cd73a28864d2",
+    "url": "/static/js/2.ac8b9752.chunk.js"
+  },
+  {
+    "revision": "e75d5b2962ff40e6c577b57c57a31cc1",
+    "url": "/static/js/2.ac8b9752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f771154c47dddb3c49a7",
+    "url": "/static/js/main.02336298.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
