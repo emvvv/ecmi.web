cabc5c83ccf603ec1b20ae34594791ed4589d918
Mary Valera <emvalera356@gmail.com>
2024-01-28 22:39:29 +0800

    add volunteer.js/css

--- src/components/ministry/Volunteer.js
+++ src/components/ministry/Volunteer.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import Footer from '../Footer';
+import './volunteer.css'
 
 
 
 function Volunteer(){
 return(
     <>
-     
+     <div className='tiles-wrapper'>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>ACCOMMODATION & ORDINANCE MINISTRY</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>USHERING & PHYSICAL ARRANGEMENT</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>MULTI MEDIA</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>COMMUNITY AFFAIRS AND PUBLIC RELATIONS</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>MEMBER'S CARE AND HOSPITALITY</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>PROPERTY</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>PRAYER</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}> SOCIAL CONCERN / COMPASSION </div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>COUPLES</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>WORSHIP</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>CHILDREN</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>OFW</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>KAINOS</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>MEN</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>WOMEN</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>LEGO</div>
+        <div className='tile' style={{backgroundColor: '#13707e'}}>E-MINISTRY</div>
+
+     </div>
    
     </>
 )
